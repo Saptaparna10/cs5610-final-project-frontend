be47d84b61b28dcf2d743c4941577dc279ec3ea3
Ritika Nair <nair.r@husky.neu.edu>
2019-04-13 00:05:30 -0400

    Tests login with service call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import {routing} from './app.routing';
+import { routing } from './app.routing';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserServiceClient } from './services/UserServiceClient';
+import { Constants } from './services/Constants';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,11 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    routing
+    routing,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserServiceClient,
+    Constants],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/UserServiceClient.ts
@@ -0,0 +1,28 @@
+
+import { Injectable } from '@angular/core';
+import { Constants } from './Constants';
+@Injectable()
+export class UserServiceClient {
+
+    constructor(private constants: Constants) { }
+
+    SERVER_API_URL = this.constants.SERVER_API_URL;
+
+
+
+    logInUser = user =>
+        fetch(this.SERVER_API_URL + '/api/login', {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json',
+            },
+            credentials: 'include'
+        }).then(response => {
+            if (response.headers.get("content-type") === null) return null;
+            else return response.json()
+        }
+        );
+
+
+} 
