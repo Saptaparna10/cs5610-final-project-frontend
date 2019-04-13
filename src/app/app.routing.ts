e32e950327bb7633cebb0b633e788a61a85a620f
Ritika Nair <nair.r@husky.neu.edu>
2019-04-12 21:09:44 -0400

    Adds sample login page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
    { path: '', component: HomeComponent },
+   { path: 'login', component: LoginComponent },
 
 ];
 export const routing = RouterModule.forRoot(appRoutes);
