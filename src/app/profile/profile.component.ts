8103671af9a4ba96cdbd12de2a5cd36fc34c2db8
Ritika Nair <nair.r@husky.neu.edu>
2019-04-13 01:04:21 -0400

    Sample profile page

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  tabOptions: string[] = ['About','Liked Recipes', 'Following'];
+  recipes:[ {
+    image_url: '',
+title: 'abc',
+description:'nnnnnnnnnnnnnnnnnnnnn',
+}]
+  selectedTabOption = this.tabOptions[0];
+  constructor() {
+   
+this.recipes=[ {
+  image_url: 'https://assets.epicurious.com/photos/5c8fc9eb1808bd2c8ed6ca7b/16:9/w_1280%2Cc_limit/Cook-This-Now-Torn-Tofu-Hero-Alt-05032019.jpg',
+title: 'abc',
+description:'nnnnnnnnnnnnnnnnnnnnn',
+}]
+//this.recipes.push(recipe );
+// {
+//   username: this.username,
+//   password: this.password
+// }
+   }
+    
+
+  ngOnInit() {
+  }
+
+}
