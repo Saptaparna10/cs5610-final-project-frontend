708d7e743f344777f007f3d266b0466a956cb9e8
preyank <preyank@ccs.neu.edu>
2019-04-15 19:01:34 -0400

    fixed conflicts in constants

--- src/app/services/Constants.ts
+++ src/app/services/Constants.ts
@@ -2,20 +2,18 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Constants {
-    //local server
-    SERVER_API_URL = 'http://localhost:8080';
+  // local server
+  SERVER_API_URL = 'http://localhost:8080';
 
-    //heroku server
-<<<<<<< HEAD
-    //SERVER_API_URL = ' https://alwayshungry.herokuapp.com';
+  // heroku server
+
+  // SERVER_API_URL = ' https://alwayshungry.herokuapp.com';
+
+  // SERVER_API_URL = ' https://alwayshungry.herokuapp.com';
+
+  // Yummly api key
+  YUMMLY_APP_ID = 'f9d4e019';
+
+  // Yummly api password
+  YUMMLY_APP_KEY = '13251a7e420fe3d168b319220dbdcbfb';
 }
-=======
-    SERVER_API_URL = ' https://alwayshungry.herokuapp.com';
-
-    //Yummly api key
-    YUMMLY_APP_ID = 'f9d4e019';
-
-    //Yummly api password
-    YUMMLY_APP_KEY = '13251a7e420fe3d168b319220dbdcbfb';
-}
->>>>>>> 4bae6fb97038404695bb13665a591ac09d46dc76
