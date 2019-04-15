fdea7b4c11119bcb08577476aa58e92ce8afec7c
preyank <preyank@ccs.neu.edu>
2019-04-15 18:48:06 -0400

    implemented register

--- src/app/services/Constants.ts
+++ src/app/services/Constants.ts
@@ -3,8 +3,8 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class Constants {
     //local server
-    //SERVER_API_URL = 'http://localhost:8080';
+    SERVER_API_URL = 'http://localhost:8080';
 
     //heroku server
-    SERVER_API_URL = ' https://alwayshungry.herokuapp.com';
+    //SERVER_API_URL = ' https://alwayshungry.herokuapp.com';
 }
