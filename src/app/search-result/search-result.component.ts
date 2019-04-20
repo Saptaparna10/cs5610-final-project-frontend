ab32bcf203dc250e228549dbc413f5b4046919cd
Ritika Nair <nair.r@husky.neu.edu>
2019-04-20 13:35:54 -0400

    nav on recipe and coll pages

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { YummlyServiceClient } from '../services/YummlyServiceClient';
+import { UserServiceClient } from '../services/UserServiceClient';
 
 @Component({
   selector: 'app-search-result',
@@ -10,34 +11,49 @@ import { YummlyServiceClient } from '../services/YummlyServiceClient';
 export class SearchResultComponent implements OnInit {
 
   searchTerm: String;
-  results:[];
+  results: [];
+  loggedInUser;
 
-  constructor(private activatedRoute: ActivatedRoute,private yummlyService: YummlyServiceClient) {
-    this.results=[];
-   }
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private yummlyService: YummlyServiceClient,
+    private userService: UserServiceClient) {
+    this.results = [];
+  }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.searchTerm = params['searchTerm'];
-      
+
       this.search();
-      
+
     });
+    this.userService.profile()
+      .then((user) => {
+        this.loggedInUser = user;
+      })
   }
 
   search(): void {
-   
+
     this.yummlyService.searchRecipeByTerm(this.searchTerm).then((searchResult) => {
-             
-      if(searchResult!=null && searchResult.matches!=null){
+
+      if (searchResult != null && searchResult.matches != null) {
         this.results = searchResult.matches;
-      console.log(this.results);
+        console.log(this.results);
       }
-       
-     });
-    
+
+    });
+
 
   }
- 
- 
+
+  logout(): void {
+    this.userService.logout()
+      .then(() => {
+        this.router.navigate(['/login']);
+      });
+  }
+
+
 }
